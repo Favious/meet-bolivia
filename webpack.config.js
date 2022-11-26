2dc8f0c12fde0b1bf77c882bdebbba61eb76d375
Favious <faviopaco@gmail.com>
2022-11-26 08:45:26 -0400

    add html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
     path: path.join(__dirname, "build"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
 };
